37183c220479efb118952ef068b5dce0c720bcfe
hexoplasmalt <rahmanh23@forest.academy>
2026-09-17 22:46:30 +0100

    DELETED bit-to-byte since it was included in binary-converter.py (all units until petabyte/pebibyte). binary-converty.py COMPLETED

--- src/binary-converter.py
+++ src/binary-converter.py
@@ -1,19 +1,35 @@
-# Unfinished
+# Unit chart
 
-inp = input("Enter value you are converting with suffix (e.g. 1 gigabyte): ").lower().split()
-to = input("Enter what you are converting to: ").lower()
+unitBase = {
+    ("b","bit","bits"):1,
+    ("nibble","nibble"):4,
+    ("B","byte","bytes"):8,
 
-# To bytes
-if inp[1] in ("gigabyte","gigabyte","gb"):
-    processing = float(inp[0])*8e+9
-elif inp[1] in ("megabyte","megabytes","mb"):
-    processing = float(inp[0])*8e+6
+    ("kb","kilobyte","kilobytes"):8e+3,
+    ("mb","megabyte","megabytes"):8e+6,
+    ("gb","gigabyte","gigabytes"):8e+9,
+    ("tb","terabyte","terabytes"):8e+12,
+    ("pb","petabyte","petabytes"):8e+15,
 
-# To final
-if to in ("megabyte","megabytes","mb"):
-    processing/=8e+6
-elif to in ("gigabyte","gigabytes","gb"):
-    processing/=8e+9
+    ("kib","kibibyte","kibibytes"):8*1024,
+    ("mib","mebibyte","mebibytes"):8*1024**2,
+    ("gib","gibibyte","gibibytes"):8*1024**3,
+    ("tib","tebibyte","tebibytes"):8*1024**4,
+    ("pib","pebibyte","pebibytes"):8*1024**5,
+}
 
+# Unsimplify unit chart
 
-print(f"{inp[0]} {inp[1]} = {processing:g} {to}")
+unitMap = {}
+for k,v in unitBase.items():
+    for i in k:
+        unitMap[i] = v
+
+# Main logic
+
+inp = input("Enter value you are converting from (e.g. 1 gigabyte): ").lower().split()
+to = input("Enter what you are converting to (e.g. megabytes): ").lower()
+
+processing = float(inp[0])*unitMap[inp[1]]
+processed = processing/unitMap[to]
+print(f"{inp[0]} {inp[1]} = {processed:g} {to}")
--- src/bit-to-byte.py
+++
@@ -1,6 +0,0 @@
-import hexotools
-
-choice = hexotools.multiChoice("Bytes to bits","Bits to bytes")
-inp = int(input("Enter value to be converted: "))
-if choice[0] == 0:print(f"{inp} = {inp*8}")
-else:print(f"{inp} = {inp/8:g}")
